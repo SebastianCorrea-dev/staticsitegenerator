5622409b7246fb4f42eb52f3a66c769a4b39380f
SebastianCorrea-dev <sebascor62@gmail.com>
2025-05-05 13:16:05 -0500

    My entire static site generator. I made an error when creating the repository.

---
+++ src/htmlnode.py
@@ -0,0 +1,52 @@
+class HTMLNode():
+    def __init__(self, tag=None, value=None, children=None, props=None):
+        self.tag = tag
+        self.value = value
+        self.children = children
+        self.props = props
+    
+    def to_html(self):
+        raise NotImplementedError("to_html method not implemented")
+    
+    def props_to_html(self):
+        new_string = ""
+        if self.props == None:
+            return new_string
+        for att in self.props:
+            new_string += f' {att}="{self.props[att]}"'
+        
+        return new_string
+    
+    def __repr__(self):
+        return f"HTMLNode({self.tag}, {self.value}, children: {self.children}, {self.props})"
+
+class LeafNode(HTMLNode):
+    def __init__(self, tag, value, props=None):
+        super().__init__(tag, value, None, props)
+    
+    def to_html(self):
+        if self.value == None:
+            raise ValueError("There is not a value")
+        elif self.tag == None:
+            return self.value
+        else:
+            return f"<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>"
+    def __repr__(self):
+        return f"LeafNode({self.tag}, {self.value}, {self.props})"
+    
+class ParentNode(HTMLNode):
+    def __init__(self, tag, children, props=None):
+        super().__init__(tag, None, children, props)
+    
+    def to_html(self):
+        if self.tag == None:
+            raise ValueError("VALUE_ERROR: There isn't a tag")
+        elif self.children == None:
+            raise ValueError("VALUE_ERROR: Children is missing a value")
+        children_html = ""
+        for child in self.children:
+            children_html += child.to_html()
+        return f"<{self.tag}{self.props_to_html()}>{children_html}</{self.tag}>"
+
+    def __repr__(self):
+        return f"ParentNode({self.tag}, children: {self.children}, {self.props})"
